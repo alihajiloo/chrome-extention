10944fe694cb561950f051db808388843ee907e0
alihajiloo <alihajiloo.ir@gmail.com>
2022-01-05 12:16:27 +0330

    style(GoogleSearch): seperate GoogleSearch component

---
+++ src/components/GoogleSearch.vue
@@ -0,0 +1,51 @@
+<template>
+  <section
+    class="bg-white bg-opacity-95 dark:bg-opacity-95 rounded-xl shadow-card relative dark:bg-gray-600 dark:shadow-favorite py-6 px-6 h-32"
+  >
+    <!---->
+    <div class="flex flex-col items-center gap-3">
+      <!-- <img
+                src="/img/Google_logo.e99543bb.svg"
+                alt="لوگوی گوگل"
+                class="w-24"
+              /> -->
+      <form
+        target="_self"
+        action="https://www.google.com/search"
+        class="w-full flex items-center gap-2"
+      >
+        <div class="w-10/12 relative">
+          <input
+            type="text"
+            name="q"
+            autocomplete="off"
+            placeholder="جستجو در گوگل"
+            tabindex="1"
+            required="required"
+            class="w-full rounded-3xl pr-10 py-1 h-9 bg-gray-50 font-medium"
+          /><svg
+            viewBox="0 0 24 24"
+            class="svg-icon svg-fill absolute right-2 top-1"
+            style="width: 1.8rem; height: 1.8rem"
+          >
+            <path
+              fill="gray"
+              stroke="none"
+              pid="0"
+              d="M9.5 3A6.5 6.5 0 0116 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5-1.5 1.5-5-5v-.79l-.27-.27A6.516 6.516 0 019.5 16 6.5 6.5 0 013 9.5 6.5 6.5 0 019.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14 14 12 14 9.5 12 5 9.5 5z"
+            ></path>
+          </svg>
+        </div>
+        <button type="submit" class="py-2 w-2/12 rounded-3xl font-extraBold">
+          جستجو
+        </button>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'GoogleSearch',
+}
+</script>
--- src/override/App.vue
+++ src/override/App.vue
@@ -50,56 +50,17 @@
         </div>
       </header>
       <main class="flex gap-x-4">
+
         <!-- start: TODO -->
         <todo />
         <!-- end: TODO -->
+
         <div class="flex-2 flex flex-col gap-y-3.5">
-          <section
-            class="bg-white bg-opacity-95 dark:bg-opacity-95 rounded-xl shadow-card relative dark:bg-gray-600 dark:shadow-favorite py-6 px-6 h-32"
-          >
-            <!---->
-            <div class="flex flex-col items-center gap-3">
-              <!-- <img
-                src="/img/Google_logo.e99543bb.svg"
-                alt="لوگوی گوگل"
-                class="w-24"
-              /> -->
-              <form
-                target="_self"
-                action="https://www.google.com/search"
-                class="w-full flex items-center gap-2"
-              >
-                <div class="w-10/12 relative">
-                  <input
-                    type="text"
-                    name="q"
-                    autocomplete="off"
-                    placeholder="جستجو در گوگل"
-                    tabindex="1"
-                    required="required"
-                    class="w-full rounded-3xl pr-10 py-1 h-9 bg-gray-50 font-medium"
-                  /><svg
-                    viewBox="0 0 24 24"
-                    class="svg-icon svg-fill absolute right-2 top-1"
-                    style="width: 1.8rem; height: 1.8rem"
-                  >
-                    <path
-                      fill="gray"
-                      stroke="none"
-                      pid="0"
-                      d="M9.5 3A6.5 6.5 0 0116 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5-1.5 1.5-5-5v-.79l-.27-.27A6.516 6.516 0 019.5 16 6.5 6.5 0 013 9.5 6.5 6.5 0 019.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14 14 12 14 9.5 12 5 9.5 5z"
-                    ></path>
-                  </svg>
-                </div>
-                <button
-                  type="submit"
-                  class="py-2 w-2/12 rounded-3xl font-extraBold"
-                >
-                  جستجو
-                </button>
-              </form>
-            </div>
-          </section>
+
+         <!-- start: GoogleSearch -->
+         <google-search/>
+         <!-- end: GoogleSearch -->
+         
           <section>
             <div class="grid grid-cols-6 grid-rows-2 gap-y-3 gap-x-2.5">
               <div class="favorite">
@@ -441,6 +402,7 @@ import '../assets/styles/index.css'
 import VuePersianDatetimePicker from 'vue-persian-datetime-picker'
 import $ from 'jquery'
 import Todo from '@/components/Todo.vue'
+import GoogleSearch from '@/components/GoogleSearch.vue'
 
 export default {
   name: 'App',
@@ -458,6 +420,7 @@ export default {
   components: {
     datePicker: VuePersianDatetimePicker,
     Todo: Todo,
+    GoogleSearch:GoogleSearch
   },
   mounted() {
     if (localStorage.getItem('tasks'))
